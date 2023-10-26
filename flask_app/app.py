1c9fb998c6794dea049516e8c0b0709038fb6588
renjithvsampcome <renjith.vs@ampcome.com>
2023-10-26 16:17:14 +0530

    added last_job_id

--- flask_app/app.py
+++ flask_app/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import requests
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from handler import get_access_token, handle_api_insta, handle_api_facebook, handle_twitter_api, get_access_url
+from handler import get_access_token, handle_api_insta, handle_api_facebook, handle_twitter_api, get_access_url, get_status
 import pangres as pg
 import os
 import logging
@@ -34,6 +34,14 @@ app = Flask(__name__)
 connection_string = os.environ.get("DB_CONNECTION_STRING")
 engine = create_engine(connection_string)
 
+@app.route('/job_status_check/<task_id>')
+def get_last_job_status(task_id):
+    status = get_status(task_id)
+    if status  == "SUCCESS":
+        return jsonify({'success': True}), 200
+    else:
+        return jsonify({"success": False}), 200
+
 @app.route('/get_user_media_insta/<code>/<id>', methods=['GET'])
 def import_instagram(code,id):
 
@@ -52,19 +60,24 @@ def import_instagram(code,id):
             response = requests.get("https://graph.instagram.com/me/media", params=params).json()
             # row tp get data of each post
             row = []
+            last_file_id = None
             if len(response['data'])!= 0:
-                handle_api_insta(row,response,token)
-                
-
+                result = handle_api_insta(row,response,token)
+                if result is not None:
+                    last_file_id = result
+                    
             if response['paging'].get("next"):
                 next_url = response['paging']['next']
             else: 
                 next_url = None
 
+            
             while next_url:
                 res = requests.get(next_url).json()
                 if len(res['data'])!= 0:
-                    handle_api_insta(row,res,token)
+                    result = handle_api_insta(row,response,token)
+                    if result is not None:
+                        last_file_id = result
 
                 if res['paging'].get("next"):
                     next_url = res['paging']['next']
@@ -79,13 +92,15 @@ def import_instagram(code,id):
                 df=df.set_index('file_id'),
                 table_name='import_instagram',
                 if_row_exists='update', schema='public', chunksize=10000)
-
-            return jsonify({'result':"Data upload is successfull"})
+            if last_file_id!=None:
+                return jsonify({'status':"Pending", "last_job_id": last_file_id}), 200
+            else:
+                return jsonify({'status': "No file to upload"}), 201
         else:
-            return jsonify({'error':"access token error"})
+            return jsonify({'error':"access token error"}) , 400
 
     except Exception as e:
-        return  jsonify({'error':f"Encounter an exception: {e}"})
+        return  jsonify({'error':f"Encounter an exception: {e}"}) , 500
     
 
 @app.route("/get_user_media_fb/<code>/<id>", methods=['GET'])
@@ -93,14 +108,16 @@ def import_facebook(code,id):
     try:
         params = {
         'fields': 'id,name,feed{name,status_type,link,permalink_url,sharedposts,created_time,full_picture,is_published,message,type,attachments{media,media_type,url},parent_id}',
-        'access_token': code  # Replace 'YOUR_ACCESS_TOKEN' with your actual access token
+        'access_token': code  
         }
         response = requests.get("https://graph.facebook.com/v18.0/me", params=params).json()
     
         row = []
-
+        last_file_id = None
         if len(response['feed']['data'])!=0:
-            handle_api_facebook(row, response['feed'])
+            result = handle_api_facebook(row, response['feed'])
+            if result is not None:
+                    last_file_id = result
 
         if 'paging' in response and response['paging'].get("next"):
                     next_url = response['paging']['next']
@@ -110,7 +127,9 @@ def import_facebook(code,id):
         while next_url:
             res = requests.get(next_url).json()
             if len(res['data'])!= 0:
-                handle_api_facebook(row,res)
+                result = handle_api_facebook(row,res)
+                if result is not None:
+                    last_file_id = result
 
             if 'paging' in res and res['paging'].get("next"):
                     next_url = res['paging']['next']
@@ -125,30 +144,33 @@ def import_facebook(code,id):
                 df=df.set_index('file_id'),
                 table_name='import_facebook',
                 if_row_exists='update', schema='public', chunksize=10000)
-        return jsonify({'result':"FB Data upload is successfull"})
+        return jsonify({'status':"Pending", "last_job_id": last_file_id}), 200
         
     except Exception as e:
-        return  jsonify({'error':f"Encounter an exception importing facebook data: {e}"})
+        return  jsonify({'error':f"Encounter an exception importing facebook data: {e}"}), 500
 
-@app.route("/get_user_media_url/",methods=['GET'])
+@app.route("/get_user_media_url_twitter/",methods=['GET'])
 def get_url():
     return get_access_url()
 
 @app.route("/get_user_media_twitter/<code>/<id>/", methods=['GET'])
 def import_twitter(code,id):
-    row = []
-    row = handle_twitter_api(row, code)
-    df = pd.DataFrame(row, columns=['file_id', 'media_type', 'media_url',"username"])
-    df['user_id'] = id
-    if len(row)!= 0:
-        pg.upsert(
-                    con=engine,
-                    df=df.set_index('file_id'),
-                    table_name='import_twitter',
-                    if_row_exists='update', schema='public', chunksize=10000)
-        return jsonify({'result':"twitter Data upload is successfull"})
-    else:
-        return jsonify({'error': "No data to upload"})
+    try:
+        row = []
+        row , r = handle_twitter_api(row, code)
+        df = pd.DataFrame(row, columns=['file_id', 'media_type', 'media_url',"username"])
+        df['user_id'] = id
+        if len(row)!= 0:
+            pg.upsert(
+                        con=engine,
+                        df=df.set_index('file_id'),
+                        table_name='import_twitter',
+                        if_row_exists='update', schema='public', chunksize=10000)
+            return jsonify({'status':"Pending", "last_job_id": r}) , 200
+        else:
+            return jsonify({'error': "No data to upload"}) , 400
+    except Exception as e:
+        return  jsonify({'error':f"Encounter an exception importing twitter data: {e}"}),500
 
 
 
--- flask_app/handler.py
+++ flask_app/handler.py
@@ -22,6 +22,11 @@ consumer_key_twitter = os.environ.get("TWITTER_CONSUMER_KEY")
 consumer_secret_twitter = os.environ.get("TWITTER_CONSUMER_SECRET")
 
 
+def get_status(task_id):
+    status = simple_app.AsyncResult(task_id, app=simple_app)
+    return str(status.state)
+
+
 def get_access_token(code):
 
     # Form data for the POST request
@@ -41,7 +46,7 @@ def get_access_token(code):
     except Exception as e:
         return jsonify({'error': str(e)})
 
-def give_file_name(name,type):
+def  give_file_name(name,type):
     if type == 'VIDEO':
         file_name = f"video_{name}.mp4"
     elif type == "IMAGE":
@@ -51,13 +56,16 @@ def give_file_name(name,type):
     return f'https://vude-bucket.blr1.digitaloceanspaces.com/test-dev/{file_name}'
 
 def handle_api_insta(row,response, token):
+    r = None
     for data in response['data']:
         caption = data['caption'] if 'caption' in data else None
         if data['media_type'] == 'VIDEO' or data['media_type'] == 'IMAGE':
-            simple_app.send_task('tasks.handle_file', kwargs={'url': data['media_url'], 'name': data['id'], 'type': data['media_type']})
             file_url = give_file_name(data['id'], data['media_type'])
-            # file_url = handle_file(data['media_url'], data['id'], data['media_type'])
-            row.append((data['id'], data["media_type"],file_url ,data["username"],data['timestamp'],False, caption))
+            if file_url:
+                r = simple_app.send_task('tasks.handle_file', kwargs={'url': data['media_url'], 'name': data['id'], 'type': data['media_type']})
+                # file_url = handle_file(data['media_url'], data['id'], data['media_type'])
+                row.append((data['id'], data["media_type"],file_url ,data["username"],data['timestamp'],False, caption))
+                
         elif data['media_type'] == 'CAROUSEL_ALBUM':
             params = {
                 'fields': 'id,media_type,media_url,username,timestamp',
@@ -65,34 +73,46 @@ def handle_api_insta(row,response, token):
             }
             res = requests.get(f"https://graph.instagram.com/{data['id']}/children", params=params).json()
             for d in res['data']:
-                simple_app.send_task('tasks.handle_file', kwargs={'url': data['media_url'], 'name': data['id'], 'type': data['media_type']})
-                file_url = give_file_name(data['id'], data['media_type'])
-                row.append((d['id'],d["media_type"],file_url ,d["username"],d['timestamp'],True,caption))
+                file_url = give_file_name(d['id'], d['media_type'])
+                if file_url:
+                    r = simple_app.send_task('tasks.handle_file', kwargs={'url': d['media_url'], 'name': d['id'], 'type': d['media_type']})
+                    row.append((d['id'],d["media_type"],file_url ,d["username"],d['timestamp'],True,caption))  
         else:
             pass
+    if r != None:
+        return r.id
+    else:
+        return None
 
 
 def handle_api_facebook(row,response):
+    r = None
     for data in response['data']:
         if data['status_type'] == "added_photos" or data['status_type'] == "added_video":
             message = data['message'] if 'message' in data else None
             if data['type'] == 'photo':
                 data['type'] = "IMAGE"
-                simple_app.send_task('tasks.handle_file', kwargs={'url': data['full_picture'], 'name': data['id'], 'type': data['type'] })
                 file_url = give_file_name(data['id'], data['type'])
-                row.append((data['id'], "IMAGE", file_url ,"username",data['created_time'],False, message))
+                if file_url:
+                    r = simple_app.send_task('tasks.handle_file', kwargs={'url': data['full_picture'], 'name': data['id'], 'type': data['type'] })
+                    row.append((data['id'], "IMAGE", file_url ,"username",data['created_time'],False, message))
+                
             elif data['type'] == "video":
                 data['type'] = 'VIDEO'
                 if 'source' in data['attachments']['data'][0]['media']:
-                    simple_app.send_task('tasks.handle_file', kwargs={'url': data['attachments']['data'][0]['media']['source'], 'name': data['id'], 'type': data['type'] })
                     file_url = give_file_name(data['id'], data['type'] )
-                    row.append((data['id'], "VIDEO", file_url ,"username",data['created_time'],False, message))
-                else:
-                    pass
+                    if file_url:
+                        r = simple_app.send_task('tasks.handle_file', kwargs={'url': data['attachments']['data'][0]['media']['source'], 'name': data['id'], 'type': data['type'] })
+                        row.append((data['id'], "VIDEO", file_url ,"username",data['created_time'],False, message))
+                        
             else: 
                 pass
         else:
             pass
+    if r != None:
+        return r.id
+    else:
+        return None
 
 #twitter
 oauth1_user_handler = tweepy.OAuth1UserHandler(
@@ -112,23 +132,26 @@ def handle_twitter_api(row, code):
 
         data = client.get_users_tweets(user.data.id,user_auth=True,tweet_fields = ['created_at', 'text', 'id', 'attachments','author_id', 'entities'], media_fields=["url","type","media_key","preview_image_url"], expansions=['attachments.media_keys', 'author_id'])
         
+        r = None
         for data in data.includes['media']:
             if data.type == 'photo':
                 data.type = "IMAGE"
-                file_url = give_file_name(data.media_key, data.type )
-                simple_app.send_task('tasks.handle_file', kwargs={'url': data.url, 'name': data.media_key, 'type': data.type })
-                row.append((data.media_key, data.type,file_url,user.data.username ))
+                file_url = give_file_name(data.media_key, data.type)
+                if file_url:
+                    r = simple_app.send_task('tasks.handle_file', kwargs={'url': data.url, 'name': data.media_key, 'type': data.type })
+                    row.append((data.media_key, data.type,file_url,user.data.username ))
             elif data.type == "video":
                 data.type = "IMAGE"
                 file_url = give_file_name(data.media_key, data.type)
-                simple_app.send_task('tasks.handle_file', kwargs={'url': data.preview_image_url, 'name': data.media_key, 'type': data.type })
-                row.append((data.media_key, data.type, file_url, user.data.username))
+                if file_url:
+                    r = simple_app.send_task('tasks.handle_file', kwargs={'url': data.preview_image_url, 'name': data.media_key, 'type': data.type })
+                    row.append((data.media_key, data.type, file_url, user.data.username))
             else:
                 pass
-        return row
+        return row , r
     except Exception as e:
         logger.info(f"Encounter an exception: {e}")
-        return  []
+        return  [] , None
         
     # print(data.includes['media'][0].preview_image_url) 
     # print(data.includes['media'][0].ype)
