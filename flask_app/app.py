34213f89461b83a712b2b2392da1ba86d811a089
renjithvsampcome <renjith.vs@ampcome.com>
2023-10-26 19:13:03 +0530

    added print

--- flask_app/app.py
+++ flask_app/app.py
@@ -86,6 +86,7 @@ def import_instagram(code,id):
             df = pd.DataFrame(row, columns=['file_id', 'media_type', 'media_url',"username","timestamp","is_album","caption"])
             df['user_id'] = id
             #save it to db
+            app.logger.info(df)
             pg.upsert(
                 con=engine,
                 df=df.set_index('file_id'),
