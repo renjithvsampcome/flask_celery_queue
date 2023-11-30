b02baee83b0bdae61c2a2c1af86484a31cb5ba8d
renjithvsampcome <renjith.vs@ampcome.com>
2023-11-30 22:30:03 +0530

    added some validation on onlyfans

--- simple_worker/test_auth_of.py
+++ simple_worker/test_auth_of.py
@@ -1,6 +1,6 @@
 
 from playwright.sync_api import Page, expect, sync_playwright
-from playwright_recaptcha import recaptchav3, recaptchav2
+from playwright_recaptcha import recaptchav3, recaptchav2 , RecaptchaNotFoundError
 from playwright_stealth import stealth_sync
 from onlyfans import onlyfans_downloader_script
 from dotenv import load_dotenv
@@ -9,15 +9,16 @@ import time
 
 load_dotenv()
 
-x_bc = None
-user_id = None
-user_agent = None
-
 
 def test_login(email, pwd, vude_id):
-    # email = args.email
-    # pwd = args.password
-    # vude_id = args.userid
+    user_agent = None
+
+    def request_handler(request):
+        headers = request.headers
+        for key, value in headers.items():
+            if key == 'user-agent':
+                global user_agent
+                user_agent = value
 
     try:
         with sync_playwright() as p:
@@ -25,15 +26,19 @@ def test_login(email, pwd, vude_id):
             context = browser.new_context()
             page = context.new_page()
             stealth_sync(page)
-            page.goto('https://onlyfans.com/')
-            with recaptchav2.SyncSolver(page,capsolver_api_key=os.environ.get('CAPSOLVER_KEY')) as solver:
-                
-                page.fill('input[name="email"]', email)
-                time.sleep(1)
-                page.fill('input[name="password"]', pwd)
-                page.click('button[type=submit]')
-                token = solver.solve_recaptcha(wait=True,image_challenge=True)
-                page.click('button[type=submit]')
+            try:
+                page.goto('https://onlyfans.com/')
+                with recaptchav2.SyncSolver(page,capsolver_api_key=os.environ.get('CAPSOLVER_KEY')) as solver:
+                    
+                    page.fill('input[name="email"]', email)
+                    time.sleep(1)
+                    page.fill('input[name="password"]', pwd)
+                    page.click('button[type=submit]')
+                    solver.solve_recaptcha(wait=True,image_challenge=True)
+                    page.click('button[type=submit]')
+                    page.on("request", request_handler)
+            except RecaptchaNotFoundError:
+                page.on("request", request_handler)
 
             time.sleep(15)
             data = context.cookies("https://onlyfans.com")  
@@ -53,17 +58,23 @@ def test_login(email, pwd, vude_id):
                 "x-bc": x_bc,
                 "user-id" : user_id,
                 "sess" : sess,
-                "user-agent" : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/120.0.6099.28 Safari/537.36',
+                "user-agent" : user_agent,
                 "vude-id": vude_id
             }
             print(clinet_side_values)
             if all(value is not None for value in clinet_side_values.values()):
                 try:
                     onlyfans_downloader_script(clinet_side_values)
+                    page.context.close()
+                    browser.close()
                 except Exception as e:
+                    page.context.close()
+                    browser.close()
                     raise Exception(f"onlyfans scrapper got crashed")
                 
             else:
+                page.context.close()
+                browser.close()
                 raise Exception(f"error while logging onlyfans, client_side_value_missing")
                 
     except Exception as e:
